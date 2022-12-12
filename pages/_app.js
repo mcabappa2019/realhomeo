c0270773e79f9566c97e613c23d3b77b10cbbf74
mcabappa2019 <mca_bappa@yahoo.co.in>
2022-12-12 22:10:51 +0530

    Added GA-4

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import Layout from '../src/components/layout';
+import galite from 'ga-lite/dist/ga-lite';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    galite('create', 'G-E1T34GYH8Q', 'auto');
+    galite('send', 'pageview');
+  }, []);
+
   return (
     <>
       <div id="portal" />
